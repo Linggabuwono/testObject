b4bf1e54d279db6bd137957421bcc17411570418
Lingga Buwono <lingga.buwono.ft18@mail.umy.ac.id>
2023-04-11 21:26:33 +0700

    membuat file index.html dan main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+//Membuat Object
+
+var dataDiri = {
+    nama : "Lingga",
+    umur : 22,
+    jk : "Laki-laki",
+    alamat : {
+        namaJalan : "Jl. Gambir",
+        no : "No 142",
+        kota : "Yogyakarta"
+    }
+};
